06d908d09226bb8a162775ecb7b192e265649880
JasleenKaur2000 <jasleenkaur46353@gmail.com>
2022-02-08 03:09:11 +0530

    week 1 task 2(a): styled navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,5 +2,25 @@ import React from "react";
 import styles from "./Navbar.module.scss";
 
 export default function Navbar() {
-  return <div className={styles.header}>Navbar</div>;
+  return (
+    <div className={styles.header}>
+      <nav className={styles.menu}>
+        <form className={styles.search_bar}>
+          <input type="text" placeholder="Search files and folders" />
+          <button>Search</button>
+        </form>
+        <ul>
+          <li className="settings">
+            <a href="#">Settings</a>
+          </li>
+          <li className="add-file">
+            <a href="#">Add files</a>
+          </li>
+          <li className="modes">
+            <a href="#">Modes</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 }
--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -2,7 +2,19 @@ import React from "react";
 import styles from "./Sidebar.scss";
 
 const Sidebar = () => {
-  return <div className={styles.header}>sidebar</div>;
+  return (
+    <div className={styles.header}>
+      <div>
+        <div>Logo Tree</div>
+        Lock
+      </div>
+      <div>
+        Resizer
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
